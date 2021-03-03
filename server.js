e8558bada267fdf7a1a0e1831504be9714e9130b
Daniel Wiesner <daniel_wiesner@gmx.de>
2021-03-03 15:51:44 +0100

    Add User model and routes for GET and POST

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
 const mongoose = require('mongoose')
+const User = require('./models/User')
 
 mongoose
-  .connect('mongodb://localhost/lean-coffee-board')
+  .connect('mongodb://localhost/lean-coffee-board', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('Connected to mongoDB')
   })
@@ -17,19 +21,17 @@ let users = []
 
 app.use(express.json())
 
-app.get('/api/users', (req, res) => {
-  res.json(users)
+app.get('/api/users', async (req, res) => {
+  res.json(await User.find())
 })
 
-app.get('/api/users/:id', (req, res) => {
+app.get('/api/users/:id', async (req, res) => {
   const { id } = req.params
-  res.json(users.find(user => user.id === id))
+  res.json(await User.findOne({ id }))
 })
 
-app.post('/api/users', (req, res) => {
-  const newUser = { ...req.body, id: uuidv4() }
-  users.push(newUser)
-  res.json(newUser)
+app.post('/api/users', async (req, res) => {
+  res.json(await User.create(req.body))
 })
 
 app.delete('/api/users/:id', (req, res) => {
