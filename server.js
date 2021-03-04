e88913380cd1351df51d8640b48e26bbb1e20f2c
Daniel Wiesner <daniel_wiesner@gmx.de>
2021-03-04 11:26:50 +0100

    Add error handling.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const { v4: uuidv4 } = require('uuid')
 const mongoose = require('mongoose')
 const User = require('./models/User')
+const Card = require('./models/Card')
 
 mongoose
   .connect('mongodb://localhost/lean-coffee-board', {
@@ -17,32 +17,54 @@ mongoose
 
 const app = express()
 
-let users = []
-
 app.use(express.json())
 
-app.get('/api/users', async (req, res) => {
-  res.json(await User.find())
+// ----------------------- USERS -----------------------
+app.get('/api/users', async (req, res, next) => {
+  res.json(await User.find().catch(next))
 })
 
-app.get('/api/users/:id', async (req, res) => {
+app.get('/api/users/:id', async (req, res, next) => {
   const { id } = req.params
-  res.json(await User.findOne({ id }))
+  res.json(await User.findOne({ id }).catch(next))
 })
 
-app.post('/api/users', async (req, res) => {
-  res.json(await User.create(req.body))
+app.post('/api/users', async (req, res, next) => {
+  res.json(await User.create(req.body).catch(next))
 })
 
-app.delete('/api/users/:id', (req, res) => {
+app.delete('/api/users/:id', async (req, res, next) => {
   const { id } = req.params
-  const indexToDelete = users.findIndex(user => user.id === id)
-  users = [...users.slice(0, indexToDelete), ...users.slice(indexToDelete + 1)]
-  res.json(users)
+  res.json(await User.deleteOne({ id }).catch(next))
 })
 
-app.get('/api/cards', (req, res) => {
-  res.json([{ title: 'First Card' }])
+// ----------------------- CARDS -----------------------
+app.get('/api/cards', async (req, res, next) => {
+  res.json(await Card.find().populate('author').catch(next))
+})
+
+app.get('/api/cards/:id', async (req, res, next) => {
+  const { id } = req.params
+  res.json(await Card.findOne({ id }).populated('author').catch(next))
+})
+
+app.post('/api/cards', async (req, res, next) => {
+  res.json(await Card.create(req.body).catch(next))
+})
+
+app.delete('/api/cards/:id', async (req, res, next) => {
+  const { id } = req.params
+  res.json(await Card.deleteOne({ id }).catch(next))
+})
+
+app.patch('/api/cards/:id', async (req, res, next) => {
+  const { id } = req.params
+  res.json(await Card.updateOne({ id }).catch(next))
+})
+
+app.use((err, req, res, next) => {
+  console.log(err.message)
+  res.json({ error: err })
 })
 
 app.listen(3000, () => {
